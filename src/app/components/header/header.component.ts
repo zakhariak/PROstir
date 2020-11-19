4756694f8288f727ca2523d7895047ce82fe8bcc
zakhariak <azakhariak@gmail.com>
2020-11-19 23:35:33 +0200

    update contacts

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, Event } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss', '../../shared/style-addition/reset.scss']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  width: number;
+  height: boolean = false;
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        // this.height = (event.url === '/home');
+        if (event.url === '/home') {
+          document.getElementById('header').style.height = '100vh';
+          this.height = true;
+        } else {
+          document.getElementById('header').style.height = 'auto';
+          this.height = false;
+        }
+      }
+    })
+  }
 
   ngOnInit(): void {
+    this.width = window.innerWidth
   }
 
 }
